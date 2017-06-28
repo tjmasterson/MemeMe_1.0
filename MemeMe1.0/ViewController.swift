9b83399c8079a55fc6e040f4a4c0c55017a9f3e4
Taylor Masterson <taylormasterson@Taylors-MacBook-Pro.local>
2017-06-28 06:31:58 -0500

    Initial Commit

---
+++ MemeMe1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe1.0
+//
+//  Created by Taylor Masterson on 6/28/17.
+//  Copyright © 2017 Taylor Masterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
